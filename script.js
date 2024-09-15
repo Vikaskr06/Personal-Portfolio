f39df1806e1399a52ee741828e395a2bfeabf54a
Vikaskr06 <vikaskr8851@gmail.com>
2024-09-15 10:18:22 +0530

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var typed = new Typed(".typewriter-text ",
+  {
+      strings: [" ",
+          "Competitive Programmer",
+          "Web Developer",
+          "B.Tech Student"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop:true,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+      
+  }
+)
